81186ef37ae5d8c0ab9e52959309aa9bac4336e1
Luis Lescano <luislescanologo@hotmail.com>
2024-01-02 19:38:22 -0300

    update

--- src/views/English.jsx
+++ src/views/English.jsx
@@ -1,15 +1,6 @@
 import React from 'react'
 import first from '../assets/eng1.jpg';
 import second from '../assets/eng2.jpg';
-import third from '../assets/eng3.jpg';
-import fourth from '../assets/eng4.jpg';
-import fiveth from '../assets/eng5.jpg';
-import sixth from '../assets/eng6.jpg';
-import seventh from '../assets/eng7.jpg';
-import eigth from '../assets/eng8.jpg';
-import nineth from '../assets/eng9.jpg';
-import tenth from '../assets/eng10.jpg';
-import eleventh from '../assets/eng11.jpg';
 
 // import third from '../assets/engl3.jpg';
 
@@ -19,16 +10,6 @@ function English() {
     <div className=' bg-black'>
       <img src={first} alt="" />
       <img src={second} alt="" />
-      <img src={third} alt="" />
-      <img src={fourth} alt="" />
-      <img src={fiveth} alt="" />
-      <img src={sixth} alt="" />
-      <img src={seventh} alt="" />
-      <img src={eigth} alt="" />
-      <img src={nineth} alt="" />
-      <img src={tenth} alt="" />
-      <img src={eleventh} alt="" />
-      {/* <img src={third} alt="" /> */}
     </div>
   )
 }
